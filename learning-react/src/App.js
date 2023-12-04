e174656e9744f27d261c30aca3661f59de7a0e54
Liliana Cortes <liliana.cortes@pop.belmont.edu>
2023-12-04 14:30:39 -0600

    Added updated css files

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
 function App(props) {
   return (
     <div className="todoapp stack-large">
